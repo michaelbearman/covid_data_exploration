ec588b54f9cb33858dd942b7356715eb9ef0654b
Michael Bearman <84734479+michaelbearman@users.noreply.github.com>
2021-06-24 23:15:02 -0400

    Cleaned script's comments

--- covid_data_exploration_script.sql
+++ covid_data_exploration_script.sql
@@ -1,35 +1,38 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 SELECT *
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
 WHERE continent IS NOT NULL
 ORDER BY 3,4
 
---SELECT *
---FROM `geometric-vim-314715.covid_data_project.covid_vaccinations`
---ORDER BY 3,4
-
---Select data that we will be using
+-- Select Data that we are going to be starting with
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
 ORDER BY 1,2
 
---Looking at total cases vs. total deaths
---Shows likelihood of dying if you contract COVID in your country
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
 WHERE location LIKE '%States%'
 ORDER BY 1,2
 
---Looking at the total cases vs population
---Shows what percentage of population got COVID
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
 
 SELECT location, date, total_cases, population, (total_cases/population)*100 AS cases_percentage
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
 WHERE location LIKE '%States%'
 ORDER BY 1,2
 
---Looking at countries with highest infection rate compared to population
+-- Countries with Highest Infection Rate compared to Population
 
 SELECT location, population, MAX(total_cases) AS highest_infection_count, MAX((total_cases/population))*100 AS percent_population_infected
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
@@ -37,7 +40,7 @@ FROM `geometric-vim-314715.covid_data_project.covid_deaths`
 GROUP BY population, location
 ORDER BY percent_population_infected DESC
 
---showing the countries with the highest death count per population
+-- Countries with Highest Death Count per Population
 
 SELECT location, MAX(total_deaths) AS highest_death_count
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
@@ -46,9 +49,9 @@ WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY highest_death_count DESC
 
---let's break things down by continent
+-- BREAKING THINGS DOWN BY CONTINENT
 
---showing the continents with the highest death count per population
+-- Showing contintents with the highest death count per population
 
 SELECT location, MAX(total_deaths) AS highest_death_count
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
@@ -57,7 +60,7 @@ WHERE continent IS NULL
 GROUP BY location
 ORDER BY highest_death_count DESC
 
---global numbers
+-- GLOBAL NUMBERS
 
 SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(new_cases)*100 AS death_percentage
 FROM `geometric-vim-314715.covid_data_project.covid_deaths`
@@ -66,7 +69,9 @@ WHERE continent is not null
 GROUP BY date
 ORDER BY 1,2
 
---looking at total population vs vaccinations
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
@@ -78,7 +83,7 @@ JOIN `geometric-vim-314715.covid_data_project.covid_vaccinations` AS vac
 WHERE dea.continent is not null
 ORDER BY 2, 3
 
---use cte
+-- Using CTE to perform Calculation on Partition By in previous query
 
 WITH pop_vs_vac
 AS 
@@ -96,7 +101,7 @@ WHERE dea.continent is not null
 Select *, (rolling_people_vaccinated/population)*100 AS percent_people_vaccinated
 FROM pop_vs_vac
 
---temp table
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 DROP TABLE IF EXISTS `geometric-vim-314715.covid_data_project.percent_population_vaccinated`;
 CREATE TABLE `geometric-vim-314715.covid_data_project.percent_population_vaccinated`
@@ -122,7 +127,7 @@ WHERE dea.continent is not null
 Select *, (rolling_people_vaccinated/population)*100 AS percent_people_vaccinated
 FROM `geometric-vim-314715.covid_data_project.percent_population_vaccinated`
 
---creating view to store data for later visualizations
+-- Creating View to store data for later visualizations
 
 CREATE VIEW `geometric-vim-314715.covid_data_project.percent_population_vaccinated_view` AS
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -134,6 +139,3 @@ JOIN `geometric-vim-314715.covid_data_project.covid_vaccinations` AS vac
     and dea.date = vac.date
 WHERE dea.continent is not null
 --ORDER BY 2, 3
-
-SELECT *
-FROM `geometric-vim-314715.covid_data_project.percent_population_vaccinated_view`
